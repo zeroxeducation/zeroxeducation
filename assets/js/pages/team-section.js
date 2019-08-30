dfcd1b26f29afc040ca6e65222f263362c76a413
UltraShieldRoger <rogerhong1996@gmail.com>
2019-08-30 23:25:06 +1000

    Fixed bugs in interaction between tabs and boards

--- assets/js/pages/team-section.js
+++ assets/js/pages/team-section.js
@@ -38,10 +38,10 @@ $(document).ready( init )
 function init()
 {
     w = $(window);
-    container = $( '#contentContainer' );
-    carousel = $( '#carouselContainer' );
-    item = $( '.carouselItem' );
-    itemLength = $( '.carouselItem' ).length;
+    container = $( '#tab1-contentContainer' );
+    carousel = $( '#tab1-carouselContainer' );
+    item = $( '.tab1-carouselItem' );
+    itemLength = $( '.tab1-carouselItem' ).length;
     fps = $('#fps');
     rY = 360 / itemLength;
     radius = Math.round( (250) / Math.tan( Math.PI / itemLength ) );
@@ -55,34 +55,98 @@ function init()
     for ( var i = 0; i < itemLength; i++ )
     {
         var $item = item.eq(i);
-        var $block = $item.find('.carouselItemInner');
-        
-//thanks @chrisgannon!        
-TweenMax.set($item, {rotationY:rY * i, z:radius, transformOrigin:"50% 50% " + -radius + "px"});
+        var $block = $item.find('.tab1-carouselItemInner');
+             
+        TweenMax.set($item, {rotationY:rY * i, z:radius, transformOrigin:"50% 50% " + -radius + "px"});
         
         animateIn( $item, $block )						
     }
     
-    // // set mouse x and y props and looper ticker
+    // set mouse x and y props and looper ticker
     window.addEventListener( "mousemove", onMouseMove, true );
     ticker = setInterval( looper, 1000/60 );
     // window.removeEventListener( "mousemove", onMouseMove, true );
 }
 
+function initTab2()
+{
+    w = $(window);
+    container = $( '#tab2-contentContainer' );
+    carousel = $( '#tab2-carouselContainer' );
+    item = $( '.tab2-carouselItem' );
+    itemLength = $( '.tab2-carouselItem' ).length;
+    fps = $('#fps');
+    rY = 360 / itemLength;
+    radius = Math.round( (250) / Math.tan( Math.PI / itemLength ) );
+    
+    // set container 3d props
+    TweenMax.set(container, {perspective:600})
+    TweenMax.set(carousel, {z:-(radius)})
+    
+    // create carousel item props
+    for ( var i = 0; i < itemLength; i++ )
+    {
+        var $item = item.eq(i);
+        var $block = $item.find('.tab2-carouselItemInner');
+        
+        TweenMax.set($item, {rotationY:rY * i, z:radius, transformOrigin:"50% 50% " + -radius + "px"});
+        
+        animateIn( $item, $block )						
+    }
+    
+    // set mouse x and y props and looper ticker
+    window.addEventListener( "mousemove", onMouseMove, true );
+    ticker = setInterval( looper, 1000/60 );
+    // window.removeEventListener( "mousemove", onMouseMove, true );
+}
+
+function initTab3()
+{
+    w = $(window);
+    container = $( '#tab3-contentContainer' );
+    carousel = $( '#tab3-carouselContainer' );
+    item = $( '.tab3-carouselItem' );
+    itemLength = $( '.tab3-carouselItem' ).length;
+    fps = $('#fps');
+    rY = 360 / itemLength;
+    radius = Math.round( (250) / Math.tan( Math.PI / itemLength ) );
+    
+    // set container 3d props
+    TweenMax.set(container, {perspective:600})
+    TweenMax.set(carousel, {z:-(radius)})
+    
+    // create carousel item props
+    for ( var i = 0; i < itemLength; i++ )
+    {
+        var $item = item.eq(i);
+        var $block = $item.find('.tab3-carouselItemInner');
+        
+        TweenMax.set($item, {rotationY:rY * i, z:radius, transformOrigin:"50% 50% " + -radius + "px"});
+        
+        animateIn( $item, $block )						
+    }
+    
+    // set mouse x and y props and looper ticker
+    window.addEventListener( "mousemove", onMouseMove, true );
+    ticker = setInterval( looper, 1000/60 );
+    // window.removeEventListener( "mousemove", onMouseMove, true );
+}
+
+
+
 function animateIn( $item, $block )
 {
-    var $nrX = 360 * getRandomInt(2);
-    var $nrY = 360 * getRandomInt(2);
+    // var $nrX = 360 * getRandomInt(2);
+    // var $nrY = 360 * getRandomInt(2);
         
-    var $nx = -(2000) + getRandomInt( 4000 )
-    var $ny = -(2000) + getRandomInt( 4000 )
-    var $nz = -4000 +  getRandomInt( 4000 )
+    // var $nx = -(2000) + getRandomInt( 4000 )
+    // var $ny = -(2000) + getRandomInt( 4000 )
+    // var $nz = -4000 +  getRandomInt( 4000 )
         
     var $s = 1.5 + (getRandomInt( 10 ) * .1)
     var $d = 1 - (getRandomInt( 8 ) * .1)
-    
     TweenMax.set( $item, { autoAlpha:1, delay:$d } )	
-    TweenMax.set( $block, { z:$nz, rotationY:$nrY, rotationX:$nrX, x:$nx, y:$ny, autoAlpha:0} )
+    // TweenMax.set( $block, { z:$nz, rotationY:$nrY, rotationX:$nrX, x:$nx, y:$ny, autoAlpha:0} )
     TweenMax.to( $block, $s, { delay:$d, rotationY:0, rotationX:0, z:0,  ease:Expo.easeInOut} )
     TweenMax.to( $block, $s-.5, { delay:$d, x:0, y:0, autoAlpha:1, ease:Expo.easeInOut} )
 }
@@ -114,7 +178,9 @@ function looper()
 {
     addX += mouseX
     TweenMax.to( carousel, 1, { rotationY:addX, rotationX:mouseY, ease:Quint.easeOut } )
-    TweenMax.set( carousel, {z:mouseZ } )
+    // TweenMax.set( carousel, {z:mouseZ } )
+    TweenMax.set( carousel, {z:-1300 } )
+    // console.log(mouseZ);
     fps.text( 'Framerate: ' + counter.tick() + '/60 FPS' )	
 }
 
